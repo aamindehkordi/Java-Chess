ddbc63cbc3fdaf8cae369d99aa671062bae132fc
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2022-10-26 15:06:30 -0700

    Moved main class to main package

--- src/com/chess/OOPChess.java
+++ src/com/chess/OOPChess.java
@@ -1,12 +1,15 @@
-package com.chess.engine;
+package com.chess;
 
 import com.chess.engine.board.Board;
+import com.chess.gui.Table;
 
 public class OOPChess {
 
     public static void main(String[] args) {
         Board board = Board.createStandardBoard();
         System.out.println(board);
+
+        Table table = new Table();
     }
 
 }
