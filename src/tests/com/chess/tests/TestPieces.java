e2ff872a75dbf322d703e72c2ef28f1e21503903
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2022-11-21 13:57:08 -0800

    Debugging Pawn Promotions

--- src/tests/com/chess/tests/TestPieces.java
+++ src/tests/com/chess/tests/TestPieces.java
@@ -378,23 +378,34 @@ public class TestPieces {
     public void testPawnPromotion() {
         final Board.Builder builder = new Board.Builder();
         // Black Layout
-        builder.setPiece(new Rook(3, Alliance.BLACK));
-        builder.setPiece(new King(22, Alliance.BLACK));
+        builder.setPiece(new Rook(3, Alliance.BLACK)); /* Rook on position 3 aka d8 */
+        builder.setPiece(new King(22, Alliance.BLACK)); /* King on position 22 aka g6 */
         // White Layout
-        builder.setPiece(new Pawn(15, Alliance.WHITE));
-        builder.setPiece(new King(52, Alliance.WHITE));
+        builder.setPiece(new Pawn(15, Alliance.WHITE)); /* Pawn on position 15 aka h7 */
+        builder.setPiece(new King(52, Alliance.WHITE)); /* King on position 52 aka e2 */
         // Set the current player
         builder.setMoveMaker(Alliance.WHITE);
         final Board board = builder.build();
-        final Move m1 = Move.MoveFactory.createMove(board, BoardUtils.getCoordinateAtPosition(
-                "h7"), BoardUtils.getCoordinateAtPosition("h8"));
+        // Make a Pawn Promotion move
+        int currentCoordinate = BoardUtils.getCoordinateAtPosition("h7");
+        int destinationCoordinate = BoardUtils.getCoordinateAtPosition("h8");
+        final Move m1 = Move.MoveFactory.createMove(board, currentCoordinate , destinationCoordinate);
         final MoveTransition t1 = board.currentPlayer().makeMove(m1);
+        // Make sure the move went through
         Assert.assertTrue(t1.getMoveStatus().isDone());
-        final Move m2 = Move.MoveFactory.createMove(t1.getTransitionBoard(), BoardUtils.getCoordinateAtPosition("d8"), BoardUtils.getCoordinateAtPosition("h8"));
+        // Take the last piece with the rook
+        currentCoordinate = BoardUtils.getCoordinateAtPosition("d8");
+        destinationCoordinate = BoardUtils.getCoordinateAtPosition("h8");
+        final Move m2 = Move.MoveFactory.createMove(t1.getTransitionBoard(), currentCoordinate, destinationCoordinate);
         final MoveTransition t2 = t1.getTransitionBoard().currentPlayer().makeMove(m2);
+        // Make sure the move went through
         Assert.assertTrue(t2.getMoveStatus().isDone());
-        final Move m3 = Move.MoveFactory.createMove(t2.getTransitionBoard(), BoardUtils.getCoordinateAtPosition("e2"), BoardUtils.getCoordinateAtPosition("d2"));
+        // Move the king one square to the left
+        currentCoordinate = BoardUtils.getCoordinateAtPosition("e2");
+        destinationCoordinate = BoardUtils.getCoordinateAtPosition("d2");
+        final Move m3 = Move.MoveFactory.createMove(t2.getTransitionBoard(), currentCoordinate, destinationCoordinate);
         final MoveTransition t3 = board.currentPlayer().makeMove(m3);
+        // Make sure the move went through
         Assert.assertTrue(t3.getMoveStatus().isDone());
     }
 
