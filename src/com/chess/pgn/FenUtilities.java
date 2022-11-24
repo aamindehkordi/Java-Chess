7f963b218dd397a215b1aea5adddff4076276abd
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2022-11-23 23:11:23 -0800

    Imported PGN FEN from someones github

--- src/com/chess/pgn/FenUtilities.java
+++ src/com/chess/pgn/FenUtilities.java
@@ -1,39 +1,182 @@
 package com.chess.pgn;
 
+
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
-import com.chess.engine.pieces.Pawn;
+import com.chess.engine.pieces.*;
+
+import static com.chess.engine.board.Board.Builder;
 
 public class FenUtilities {
 
     private FenUtilities() {
-        throw new RuntimeException("You cannot instantiate me!");
+        throw new RuntimeException("Not Instantiable!");
     }
 
-    public static Board createGameFromFen(final String fen) {
-        return null;
+    public static Board createGameFromFEN(final String fenString) {
+        return parseFEN(fenString);
     }
 
-    public static String createFenFromGame(final Board board) {
-        return calculateBoardText(board) + " " + calculateCurrentPlayerText(board) + " " +
-                calculateCastlingAvailabilityText(board) + " " + calculateEnPassantSquare(board) + " "
-                + calculateMoveNumber(board);
+    public static String createFENFromGame(final Board board) {
+        return calculateBoardText(board) + " " +
+               calculateCurrentPlayerText(board) + " " +
+               calculateCastleText(board) + " " +
+               calculateEnPassantSquare(board) + " " +
+               "0 1";
     }
 
-    private static String calculateMoveNumber(Board board) {
-        return String.valueOf(board.currentPlayer().getOpponent().getMoveNumber());
-    }
-
-    private static String calculateBoardText(Board board) {
-        final StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
-            final String tileText = board.getTile(i).toString();
-            builder.append(tileText);
-            if ((i + 1) % BoardUtils.NUM_TILES_PER_ROW == 0) {
-                builder.append("/");
+    private static Board parseFEN(final String fenString) {
+        final String[] fenPartitions = fenString.trim().split(" ");
+        final Builder builder = new Builder();
+        final boolean whiteKingSideCastle = whiteKingSideCastle(fenPartitions[2]);
+        final boolean whiteQueenSideCastle = whiteQueenSideCastle(fenPartitions[2]);
+        final boolean blackKingSideCastle = blackKingSideCastle(fenPartitions[2]);
+        final boolean blackQueenSideCastle = blackQueenSideCastle(fenPartitions[2]);
+        final String gameConfiguration = fenPartitions[0];
+        final char[] boardTiles = gameConfiguration.replaceAll("/", "")
+                .replaceAll("8", "--------")
+                .replaceAll("7", "-------")
+                .replaceAll("6", "------")
+                .replaceAll("5", "-----")
+                .replaceAll("4", "----")
+                .replaceAll("3", "---")
+                .replaceAll("2", "--")
+                .replaceAll("1", "-")
+                .toCharArray();
+        int i = 0;
+        while (i < boardTiles.length) {
+            switch (boardTiles[i]) {
+                case 'r':
+                    builder.setPiece(new Rook(i, Alliance.BLACK));
+                    i++;
+                    break;
+                case 'n':
+                    builder.setPiece(new Knight(i, Alliance.BLACK));
+                    i++;
+                    break;
+                case 'b':
+                    builder.setPiece(new Bishop(i, Alliance.BLACK));
+                    i++;
+                    break;
+                case 'q':
+                    builder.setPiece(new Queen(i, Alliance.BLACK));
+                    i++;
+                    break;
+                case 'k':
+                    final boolean isCastled = !blackKingSideCastle && !blackQueenSideCastle;
+                    builder.setPiece(new King(Alliance.BLACK, i, blackKingSideCastle, blackQueenSideCastle));
+                    i++;
+                    break;
+                case 'p':
+                    builder.setPiece(new Pawn(i, Alliance.BLACK));
+                    i++;
+                    break;
+                case 'R':
+                    builder.setPiece(new Rook(i, Alliance.WHITE));
+                    i++;
+                    break;
+                case 'N':
+                    builder.setPiece(new Knight(i, Alliance.WHITE));
+                    i++;
+                    break;
+                case 'B':
+                    builder.setPiece(new Bishop(i, Alliance.WHITE));
+                    i++;
+                    break;
+                case 'Q':
+                    builder.setPiece(new Queen(i, Alliance.WHITE));
+                    i++;
+                    break;
+                case 'K':
+                    builder.setPiece(new King(Alliance.WHITE, i, whiteKingSideCastle, whiteQueenSideCastle));
+                    i++;
+                    break;
+                case 'P':
+                    builder.setPiece(new Pawn(i, Alliance.WHITE));
+                    i++;
+                    break;
+                case '-':
+                    i++;
+                    break;
+                default:
+                    throw new RuntimeException("Invalid FEN String " +gameConfiguration);
             }
         }
-        return builder.toString().replaceAll("--------", "8")
+        builder.setMoveMaker(moveMaker(fenPartitions[1]));
+        return builder.build();
+    }
+
+    private static Alliance moveMaker(final String moveMakerString) {
+        if(moveMakerString.equals("w")) {
+            return Alliance.WHITE;
+        } else if(moveMakerString.equals("b")) {
+            return Alliance.BLACK;
+        }
+        throw new RuntimeException("Invalid FEN String " +moveMakerString);
+    }
+
+    private static boolean whiteKingSideCastle(final String fenCastleString) {
+        return fenCastleString.contains("K");
+    }
+
+    private static boolean whiteQueenSideCastle(final String fenCastleString) {
+        return fenCastleString.contains("Q");
+    }
+
+    private static boolean blackKingSideCastle(final String fenCastleString) {
+        return fenCastleString.contains("k");
+    }
+
+    private static boolean blackQueenSideCastle(final String fenCastleString) {
+        return fenCastleString.contains("q");
+    }
+
+    private static String calculateCastleText(final Board board) {
+        final StringBuilder builder = new StringBuilder();
+        if(board.whitePlayer().isKingSideCastleCapable()) {
+            builder.append("K");
+        }
+        if(board.whitePlayer().isQueenSideCastleCapable()) {
+            builder.append("Q");
+        }
+        if(board.blackPlayer().isKingSideCastleCapable()) {
+            builder.append("k");
+        }
+        if(board.blackPlayer().isQueenSideCastleCapable()) {
+            builder.append("q");
+        }
+        final String result = builder.toString();
+
+        return result.isEmpty() ? "-" : result;
+    }
+
+    private static String calculateEnPassantSquare(final Board board) {
+        final Pawn enPassantPawn = board.getEnPassantPawn();
+        if(enPassantPawn != null) {
+            return BoardUtils.getPositionAtCoordinate(enPassantPawn.getPiecePosition() +
+                    (8) * enPassantPawn.getPieceAlliance().getOppositeDirection());
+        }
+        return "-";
+    }
+
+    private static String calculateBoardText(final Board board) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+            final String tileText = board.getTile(i) == null ? "-" :
+                    board.getTile(i).getPiece().getPieceAlliance().isWhite() ? board.getTile(i).toString() :
+                    board.getTile(i).toString().toLowerCase();
+            builder.append(tileText);
+        }
+        builder.insert(8, "/");
+        builder.insert(17, "/");
+        builder.insert(26, "/");
+        builder.insert(35, "/");
+        builder.insert(44, "/");
+        builder.insert(53, "/");
+        builder.insert(62, "/");
+        return builder.toString()
+                .replaceAll("--------", "8")
                 .replaceAll("-------", "7")
                 .replaceAll("------", "6")
                 .replaceAll("-----", "5")
@@ -43,36 +186,8 @@ public class FenUtilities {
                 .replaceAll("-", "1");
     }
 
-    private static String calculateEnPassantSquare(Board board) {
-        final Pawn enPassantPawn = board.getEnPassantPawn();
-        if(enPassantPawn != null) {
-            return BoardUtils.getPositionAtCoordinate(enPassantPawn.getPiecePosition() +
-                    (8) * enPassantPawn.getPieceAlliance().getOppositeDirection());
-        }
-        return "-";
-    }
-
-    private static String calculateCastlingAvailabilityText(Board board) {
-        final StringBuilder builder = new StringBuilder();
-        if (board.whitePlayer().getPlayerKing().isKingSideCastleCapable()) {
-            builder.append("K");
-        }
-        if (board.whitePlayer().getPlayerKing().isQueenSideCastleCapable()) {
-            builder.append("Q");
-        }
-        if (board.blackPlayer().getPlayerKing().isKingSideCastleCapable()) {
-            builder.append("k");
-        }
-        if (board.blackPlayer().getPlayerKing().isQueenSideCastleCapable()) {
-            builder.append("q");
-        }
-        final String result = builder.toString();
-        return result.isEmpty() ? "-" : result;
-    }
-
-    private static String calculateCurrentPlayerText(Board board) {
+    private static String calculateCurrentPlayerText(final Board board) {
         return board.currentPlayer().toString().substring(0, 1).toLowerCase();
     }
-
 
 }
