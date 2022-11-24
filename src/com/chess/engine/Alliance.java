7749a1542c6ae4d19f8e6070f3d2b52309e9ed84
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2022-11-24 15:49:13 -0800

    added hotspots for each piece for evaluations

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -33,6 +33,36 @@ public enum Alliance {
         public boolean isPawnPromotionSquare(int position) {
             return BoardUtils.FIRST_ROW[position];
         }
+
+        @Override
+        public int pawnBonus(int position) {
+            return WHITE_PAWN_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int knightBonus(int position) {
+            return WHITE_KNIGHT_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int bishopBonus(int position) {
+            return WHITE_BISHOP_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int rookBonus(int position) {
+            return WHITE_ROOK_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int queenBonus(int position) {
+            return WHITE_QUEEN_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int kingBonus(int position) {
+            return WHITE_KING_PREFERRED_COORDINATES[position];
+        }
     },
 
     /** Black */
@@ -60,6 +90,36 @@ public enum Alliance {
         @Override
         public boolean isPawnPromotionSquare(int position) {
             return BoardUtils.EIGHTH_ROW[position];
+        }
+
+        @Override
+        public int pawnBonus(int position) {
+            return BLACK_PAWN_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int knightBonus(int position) {
+            return BLACK_KNIGHT_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int bishopBonus(int position) {
+            return BLACK_BISHOP_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int rookBonus(int position) {
+            return BLACK_ROOK_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int queenBonus(int position) {
+            return BLACK_QUEEN_PREFERRED_COORDINATES[position];
+        }
+
+        @Override
+        public int kingBonus(int position) {
+            return BLACK_KING_PREFERRED_COORDINATES[position];
         }
     };
 
@@ -103,4 +163,150 @@ public enum Alliance {
     public int getOppositeDirection() {
         return -1 * this.getDirection();
     }
+
+    //TODO ADD COMMENTS
+
+    public abstract int pawnBonus(int position);
+
+    public abstract int knightBonus(int position);
+
+    public abstract int bishopBonus(int position);
+
+    public abstract int rookBonus(int position);
+
+    public abstract int queenBonus(int position);
+
+    public abstract int kingBonus(int position);
+
+    private final static int[] WHITE_PAWN_PREFERRED_COORDINATES = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            75, 75, 75, 75, 75, 75, 75, 75,
+            25, 25, 29, 29, 29, 29, 25, 25,
+            5,  5, 10, 55, 55, 10,  5,  5,
+            0,  0,  0, 20, 20,  0,  0,  0,
+            5, -5,-10,  0,  0,-10, -5,  5,
+            5, 10, 10,-20,-20, 10, 10,  5,
+            0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    private final static int[] BLACK_PAWN_PREFERRED_COORDINATES = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 10, 10,-20,-20, 10, 10,  5,
+            5, -5,-10,  0,  0,-10, -5,  5,
+            0,  0,  0, 20, 20,  0,  0,  0,
+            5,  5, 10, 55, 55, 10,  5,  5,
+            25, 25, 29, 29, 29, 29, 25, 25,
+            75, 75, 75, 75, 75, 75, 75, 75,
+            0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    private final static int[] WHITE_KNIGHT_PREFERRED_COORDINATES = {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50
+    };
+
+    private final static int[] BLACK_KNIGHT_PREFERRED_COORDINATES = {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50,
+    };
+
+    private final static int[] WHITE_BISHOP_PREFERRED_COORDINATES = {
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20
+    };
+
+    private final static int[] BLACK_BISHOP_PREFERRED_COORDINATES = {
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20,
+    };
+
+    private final static int[] WHITE_ROOK_PREFERRED_COORDINATES = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 20, 20, 20, 20, 20, 20,  5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            0,  0,  0,  5,  5,  0,  0,  0
+    };
+
+    private final static int[] BLACK_ROOK_PREFERRED_COORDINATES = {
+            0,  0,  0,  5,  5,  0,  0,  0,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            5, 20, 20, 20, 20, 20, 20,  5,
+            0,  0,  0,  0,  0,  0,  0,  0,
+    };
+
+    private final static int[] WHITE_QUEEN_PREFERRED_COORDINATES = {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -5,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+    };
+
+    private final static int[] BLACK_QUEEN_PREFERRED_COORDINATES = {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+    };
+
+    private final static int[] WHITE_KING_PREFERRED_COORDINATES = {
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -20,-30,-30,-40,-40,-30,-30,-20,
+            -10,-20,-20,-20,-20,-20,-20,-10,
+            20, 20,  0,  0,  0,  0, 20, 20,
+            20, 30, 10,  0,  0, 10, 30, 20
+    };
+
+    private final static int[] BLACK_KING_PREFERRED_COORDINATES = {
+            20, 30, 10,  0,  0, 10, 30, 20,
+            20, 20,  0,  0,  0,  0, 20, 20,
+            -10,-20,-20,-20,-20,-20,-20,-10,
+            -20,-30,-30,-40,-40,-30,-30,-20,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30
+    };
 }
