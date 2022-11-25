432e00d16ebcf3d0f37564b02c7d343fc1ae72e5
nathanmaldonado <natethegreat22916@gmail.com>
2022-11-24 17:00:46 -0800

    x

--- src/com/chess/engine/player/ai/AlphaBetaWithMoveOrdering.java
+++ src/com/chess/engine/player/ai/AlphaBetaWithMoveOrdering.java
@@ -14,9 +14,10 @@ import java.util.Observable;
 import java.util.stream.Collectors;
 
 import static com.chess.engine.board.BoardUtils.mvvlva;
-
+/**
+ * AI that uses the alpha-beta pruning algorithm with move ordering to determine the best move.
+ */
 public class AlphaBetaWithMoveOrdering extends Observable implements MoveStrategy {
-//TODO ADD COMMENTS
 
     private final BoardEvaluator evaluator;
     private final int searchDepth;
@@ -27,8 +28,15 @@ public class AlphaBetaWithMoveOrdering extends Observable implements MoveStrateg
     private int quiescenceCount;
     private int cutOffsProduced;
 
+
+    /**
+     * Move sorter is an enum that determines how the moves are sorted.
+     */
     private enum MoveSorter {
 
+        /**
+         * SORT is an enum that sorts the moves by the MVV/LVA heuristic.
+         */
         SORT {
 
             @Override
@@ -38,6 +46,9 @@ public class AlphaBetaWithMoveOrdering extends Observable implements MoveStrateg
         };
 
         // Using standard java libraries
+        /**
+         * Comparator that sorts the moves by the MVV/LVA heuristic.
+         */
         public static Comparator<Move> SMART_SORT = new Comparator<Move>() {
             @Override
                 public int compare(final Move move1, final Move move2) {
@@ -45,9 +56,20 @@ public class AlphaBetaWithMoveOrdering extends Observable implements MoveStrateg
             }
         };
 
+        /**
+         * Sorts the moves.
+         * @param moves the moves to be sorted
+         * @return the sorted moves
+         */
         abstract Collection<Move> sort(Collection<Move> moves);
     }
 
+    /**
+     * Constructor for the AlphaBetaWithMoveOrdering class.
+     * @param searchDepth the depth to search to
+     * @param quiescenceFactor the quiescence factor to use
+     * queisenceFactor is the number of moves to search before using the quiescence search
+     */
     public AlphaBetaWithMoveOrdering(final int searchDepth,
                                      final int quiescenceFactor) {
         this.evaluator = StandardBoardEvaluator.get();
@@ -69,113 +91,130 @@ public class AlphaBetaWithMoveOrdering extends Observable implements MoveStrateg
         return this.boardsEvaluated;
     }
 
+
     @Override
     public Move execute(final Board board) {
-        final long startTime = System.currentTimeMillis();
-        final Player currentPlayer = board.currentPlayer();
-        final Alliance alliance = currentPlayer.getAlliance();
-        Move bestMove = MoveFactory.getNullMove();
-        int highestSeenValue = Integer.MIN_VALUE;
-        int lowestSeenValue = Integer.MAX_VALUE;
-        int currentValue;
-        int moveCounter = 1;
-        final int numMoves = this.moveSorter.sort(board.currentPlayer().getLegalMoves()).size();
-        System.out.println(board.currentPlayer() + " THINKING with depth = " + this.searchDepth);
-        System.out.println("\tOrdered moves! : " + this.moveSorter.sort(board.currentPlayer().getLegalMoves()));
+        final long startTime = System.currentTimeMillis();      // start the timer
+        final Player currentPlayer = board.currentPlayer();     // get the current player
+        final Alliance alliance = currentPlayer.getAlliance();  // get the alliance of the current player
+        Move bestMove = MoveFactory.getNullMove();              // set the best move to null
+        int highestSeenValue = Integer.MIN_VALUE;               // set the highest seen value to the minimum integer value
+        int lowestSeenValue = Integer.MAX_VALUE;                // set the lowest seen value to the maximum integer value
+        int currentValue;                                       // declare the current value
+        int moveCounter = 1;                                    // declare the move counter and set it to 1
+
+        final int numMoves = this.moveSorter.sort(board.currentPlayer().getLegalMoves()).size();    // get the number of legal moves
+        System.out.println(board.currentPlayer() + " THINKING with depth = " + this.searchDepth);   // print the current player and the depth
+        System.out.println("\tOrdered moves! : " + this.moveSorter.sort(board.currentPlayer().getLegalMoves()));      // print the ordered moves
         for (final Move move : this.moveSorter.sort(board.currentPlayer().getLegalMoves())) {
-            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
-            this.quiescenceCount = 0;
-            final String s;
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);     // make the move
+            this.quiescenceCount = 0;                                                       // reset the quiescence count
+            final String s;                                                                 // declare the string
             if (moveTransition.getMoveStatus().isDone()) {
-                final long candidateMoveStartTime = System.nanoTime();
-                currentValue = alliance.isWhite() ?
-                        min(moveTransition.getTransitionBoard(), this.searchDepth - 1, highestSeenValue, lowestSeenValue) :
-                        max(moveTransition.getTransitionBoard(), this.searchDepth - 1, highestSeenValue, lowestSeenValue);
+                final long candidateMoveStartTime = System.nanoTime();                      // start the timer
+                currentValue = alliance.isWhite() ?                                         // get the current value
+                        min(moveTransition.getTransitionBoard(), this.searchDepth - 1, highestSeenValue, lowestSeenValue) : // if the alliance is white
+                        max(moveTransition.getTransitionBoard(), this.searchDepth - 1, highestSeenValue, lowestSeenValue);  // if the alliance is black
                 if (alliance.isWhite() && currentValue > highestSeenValue) {
-                    highestSeenValue = currentValue;
-                    bestMove = move;
+                    highestSeenValue = currentValue;              // set the highest seen value to the current value
+                    bestMove = move;                              // set the best move to the current move
                     //setChanged();
                     //notifyObservers(bestMove);
                 }
                 else if (alliance.isBlack() && currentValue < lowestSeenValue) {
-                    lowestSeenValue = currentValue;
-                    bestMove = move;
+                    lowestSeenValue = currentValue;               // set the lowest seen value to the current value
+                    bestMove = move;                              // set the best move to the current move
                     //setChanged();
                     //notifyObservers(bestMove);
                 }
-                final String quiescenceInfo = " [h: " +highestSeenValue+ " l: " +lowestSeenValue+ "] q: " +this.quiescenceCount;
-                s = "\t" + toString() + "(" +this.searchDepth+ "), m: (" +moveCounter+ "/" +numMoves+ ") " + move + ", best:  " + bestMove
+                final String quiescenceInfo = " [h: " +highestSeenValue+ " l: " +lowestSeenValue+ "] q: " +this.quiescenceCount;   // get the quiescence info
+                s = "\t" + toString() + "(" +this.searchDepth+ "), m: (" +moveCounter+ "/" +numMoves+ ") " + move + ", best:  " + bestMove // get the string
 
-                        + quiescenceInfo + ", t: " +calculateTimeTaken(candidateMoveStartTime, System.nanoTime());
+                        + quiescenceInfo + ", t: " +calculateTimeTaken(candidateMoveStartTime, System.nanoTime());                 // get the time taken
             } else {
-                s = "\t" + toString() + ", m: (" +moveCounter+ "/" +numMoves+ ") " + move + " is illegal, best: " +bestMove;
+                s = "\t" + toString() + ", m: (" +moveCounter+ "/" +numMoves+ ") " + move + " is illegal, best: " +bestMove;       // get the string
             }
-            System.out.println(s);
-            setChanged();
-            notifyObservers(s);
-            moveCounter++;
+            System.out.println(s);      // print the string
+            setChanged();               // set the changed
+            notifyObservers(s);         // notify the observers
+            moveCounter++;              // increment the move counter
         }
-        this.executionTime = System.currentTimeMillis() - startTime;
+        this.executionTime = System.currentTimeMillis() - startTime;    // get the execution time
         System.out.printf("%s SELECTS %s [#boards evaluated = %d, time taken = %d ms, eval rate = %.1f cutoffCount = %d prune percent = %.2f\n", board.currentPlayer(),
                 bestMove, this.boardsEvaluated, this.executionTime, (1000 * ((double)this.boardsEvaluated/this.executionTime)), this.cutOffsProduced, 100 * ((double)this.cutOffsProduced/this.boardsEvaluated));
-        return bestMove;
+        return bestMove;        // return the best move
     }
 
-    public int max(final Board board,
-                   final int depth,
-                   final int highest,
-                   final int lowest) {
+    public int max(final Board board,                       // the board
+                   final int depth,                         // the depth
+                   final int highest,                       // the highest value
+                   final int lowest) {                      // the lowest value
         if (depth == 0 || BoardUtils.isEndGame(board)) {
-            this.boardsEvaluated++;
-            return this.evaluator.evaluate(board, depth);
+            this.boardsEvaluated++;                         // increment the boards evaluated
+            return this.evaluator.evaluate(board, depth);   // return the evaluation of the board
         }
-        int currentHighest = highest;
-        for (final Move move : this.moveSorter.sort((board.currentPlayer().getLegalMoves()))) {
-            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
-            if (moveTransition.getMoveStatus().isDone()) {
-                currentHighest = Math.max(currentHighest, min(moveTransition.getTransitionBoard(),
-                        calculateQuiescenceDepth(board, move, depth), currentHighest, lowest));
-                if (lowest <= currentHighest) {
-                    this.cutOffsProduced++;
+        int currentHighest = highest;                       // set the current highest to the highest
+        for (final Move move : this.moveSorter.sort((board.currentPlayer().getLegalMoves()))) {         // for each move in the ordered moves
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);                 // make the move
+            if (moveTransition.getMoveStatus().isDone()) {                                              // if the move is done
+                currentHighest = Math.max(currentHighest, min(moveTransition.getTransitionBoard(),      // set the current highest to the maximum of the current highest and the minimum of the transition board
+                        calculateQuiescenceDepth(board, move, depth), currentHighest, lowest));         // and the quiescence depth and the current highest and the lowest
+                if (lowest <= currentHighest) {         // if the lowest is less than or equal to the current highest
+                    this.cutOffsProduced++;             // increment the cut-offs produced
                     break;
                 }
             }
         }
-        return currentHighest;
+        return currentHighest;                          // return the current highest
     }
 
-    public int min(final Board board,
-                   final int depth,
-                   final int highest,
-                   final int lowest) {
-        if (depth == 0 || BoardUtils.isEndGame(board)) {
-            this.boardsEvaluated++;
-            return this.evaluator.evaluate(board, depth);
+    public int min(final Board board,                   // the board
+                   final int depth,                     // the depth
+                   final int highest,                   // the highest value
+                   final int lowest) {                  // the lowest value
+        if (depth == 0 || BoardUtils.isEndGame(board)) {    // if the depth is 0 or the board is in an end game
+            this.boardsEvaluated++;                         // increment the boards evaluated
+            return this.evaluator.evaluate(board, depth);   // return the evaluation of the board
         }
-        int currentLowest = lowest;
+        int currentLowest = lowest;                         // set the current lowest to the lowest
         for (final Move move : this.moveSorter.sort((board.currentPlayer().getLegalMoves()))) {
-            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
-            if (moveTransition.getMoveStatus().isDone()) {
-                currentLowest = Math.min(currentLowest, max(moveTransition.getTransitionBoard(),
-                        calculateQuiescenceDepth(board, move, depth), highest, currentLowest));
-                if (currentLowest <= highest) {
-                    this.cutOffsProduced++;
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);          // make the move
+            if (moveTransition.getMoveStatus().isDone()) {                                       // if the move is done
+                currentLowest = Math.min(currentLowest, max(moveTransition.getTransitionBoard(), // set the current lowest to the minimum of the current lowest and the maximum of the transition board
+                        calculateQuiescenceDepth(board, move, depth), highest, currentLowest));  // and the quiescence depth and the highest and the current lowest
+                if (currentLowest <= highest) {      // if the current lowest is less than or equal to the highest
+                    this.cutOffsProduced++;          // increment the cut-offs produced
                     break;
                 }
             }
         }
-        return currentLowest;
+        return currentLowest;                        // return the current lowest
     }
 
-    private int calculateQuiescenceDepth(final Board board,
-                                         final Move move,
-                                         final int depth) {
-        return depth - 1;
+    /**
+     * Calculates the quiescence depth.
+     *
+     * @param board the board
+     * @param move the move
+     * @param depth the depth
+     * @return the quiescence depth
+     */
+    private int calculateQuiescenceDepth(final Board board,  // the board
+                                         final Move move,    // the move
+                                         final int depth) {  // the depth
+        return depth - 1;                                    // return the depth minus 1
     }
 
-    private static String calculateTimeTaken(final long start, final long end) {
-        final long timeTaken = (end - start) / 1000000;
-        return timeTaken + " ms";
+    /**
+     * Calculates the time taken.
+     *
+     * @param start the start time
+     * @param end the end time
+     * @return timeTaken
+     */
+    private static String calculateTimeTaken(final long start, final long end) {  // the start time and the end time
+        final long timeTaken = (end - start) / 1000000;                           // get the time taken
+        return timeTaken + " ms";                                                 // return the time taken
     }
 
 
